c648e7e3b9036f5bfe41fd602c0c771f69ddfa4c
ferbrio1 <146587541+ferbrio1@users.noreply.github.com>
2023-11-13 11:39:10 -0600

    Add files via upload

---
+++ WebApi/Controllers/AutorController.cs
@@ -0,0 +1,38 @@
+﻿using ExamenBiblioteca.Models;
+using ExamenBiblioteca.Operaciones;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private AutorDAO autorDAO = new AutorDAO();
+
+        /**
+         *EndPoint para obtener todos los  autores
+         */
+        [HttpGet("autores")]
+        public ActionResult<List<Autor>> GetAutores()
+        {
+            List<Autor> autores = autorDAO.seleccionarTodos();
+
+            if (autores == null || autores.Count == 0)
+            {
+                return null; // Puedes devolver un resultado null si la lista está vacía o no se encontraron profesores.
+            }
+            return autores;
+        }
+
+        /**
+         *EndPoint para insertar un autor
+         */
+        [HttpPost("autor")]
+        public bool insertarProfesor([FromBody] Autor autor)
+        {
+            return autorDAO.insertar(autor.Id, autor.Nombre);
+        }
+    }
+}
---
+++ WebApi/Controllers/LibroController.cs
@@ -0,0 +1,56 @@
+﻿using ExamenBiblioteca.Models;
+using ExamenBiblioteca.Operaciones;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private LibroDAO libroDAO = new LibroDAO();
+
+        /**
+         *EndPoint para obtener un libro  especifico
+         */
+        [HttpGet("libro")]
+        public Libro getAutor(int id)
+        {
+            return libroDAO.seleccionar(id);
+        }
+
+        /**
+         *EndPoint para obtener un libro  especifico
+         */
+        [HttpGet("libroT")]
+        public Libro getAutor(string titulo)
+        {
+            return libroDAO.seleccionarTitulo(titulo);
+        }
+
+        /**
+         *EndPoint para obtener todos los  autores
+         */
+        [HttpGet("libros")]
+        public ActionResult<List<LibroAutor>> GetLibros()
+        {
+            List<LibroAutor> libros = libroDAO.seleccionarTodos();
+
+            if (libros == null || libros.Count == 0)
+            {
+                return null; // Puedes devolver un resultado null si la lista está vacía o no se encontraron profesores.
+            }
+            return libros;
+        }
+
+        /**
+         *EndPoint para insertar un autor
+         */
+        [HttpPost("libro")]
+        public bool insertarLibro([FromBody] Libro libro)
+        {
+            return libroDAO.insertar(libro.Id, libro.Titulo, (int)libro.Capitulos, (int)libro.Paginas, (int)libro.Precio, (int)libro.AutorId);
+        }
+    }
+}
